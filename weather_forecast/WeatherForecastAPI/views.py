e8baa348fd0696ef8d7ea79c1329f50c194957e9
V1Ro-Dev <matveymdmd@mail.ru>
2024-08-13 20:29:27 +0300

    Изменения обработке get запроса + Добавлен файл config для вставки api-кючей

--- weather_forecast/WeatherForecastAPI/views.py
+++ weather_forecast/WeatherForecastAPI/views.py
@@ -1,15 +1,15 @@
 from django.views.generic import View
-from django.http import JsonResponse
+from django.http import HttpResponse
+from .config import YANDEX_API_KEY, OPENWEATHERMAP_API_KEY
 import requests
-from typing import Dict
+import json
 
 
 def get_coordinates(city: str) -> list[str]:
-    KEY = '18a96da4a6867b5c0fbe4297cdfdf9a8'
     url = 'http://api.openweathermap.org/geo/1.0/direct'
     params = {
         'q': city,
-        'appid': KEY
+        'appid': OPENWEATHERMAP_API_KEY,
     }
     response = requests.get(url, params).json()
     lat = response[0]['lat']
@@ -44,10 +44,8 @@ class CheckConditionView(View):
 
     def get_weather_response(self, lat, lon):
 
-        access_key = "7874040d-7b9d-4553-a7c8-5b9e00b1feed"
-
         headers = {
-            "X-Yandex-Weather-Key": access_key
+            "X-Yandex-Weather-Key": YANDEX_API_KEY
         }
 
         query = self.exec_graphql_query(lat, lon)
@@ -56,14 +54,27 @@ class CheckConditionView(View):
 
         return response.json()['data']['weatherByPoint']['forecast']['days'][0]['hours']
 
+    @staticmethod
+    def transform_weather_data_to_set(forecast_conditions: list) -> set:
+        for dic in forecast_conditions:
+            if dic['condition'] in ['PARTLY_CLOUDY', 'CLOUDY']:
+                dic['condition'] = 'OVERCAST'
+            elif dic['condition'] in ['LIGHT_RAIN', 'HEAVY_RAIN', 'SHOWERS']:
+                dic['condition'] = 'RAIN'
+            elif dic['condition'] in ['LIGHT_SNOW', 'SNOWFALL']:
+                dic['condition'] = 'SNOW'
+            elif dic['condition'] in ['THUNDERSTORM_WITH_RAIN', 'THUNDERSTORM_WITH_HAIL']:
+                dic['condition'] = 'THUNDERSTORM'
+        return {event['condition'] for event in forecast_conditions}
+
     def get(self, request):
-        requested_params = request.GET
+        requested_params = json.loads(request.GET.get('data'))
         final_response = []
 
         for dic in requested_params:
             lat, lon = get_coordinates(dic['city'])
             weather = self.get_weather_response(lat, lon)
-            forecast_conditions = set(event['condition'] for event in weather)
+            forecast_conditions = self.transform_weather_data_to_set(weather)
 
             real_dic = {
                 'city': dic['city'],
@@ -75,4 +86,4 @@ class CheckConditionView(View):
             else:
                 final_response.append(real_dic)
 
-            return JsonResponse(final_response)
+            return HttpResponse(json.dumps(final_response, ensure_ascii=False))
